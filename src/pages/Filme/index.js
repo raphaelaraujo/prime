526365642f154591d3b8122816064d5cf2d16426
raphaelaraujo <raphaelaraujo075@gmail.com>
2022-06-17 13:36:26 -0300

    Projeto Prime Flix

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,61 +1,86 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './index.css';
 import api from '../../services/api';
 
+import { toast } from 'react-toastify';
+
 //Link: https://api.themoviedb.org/3/movie/338953?api_key=b08cd9bdb025e6a250bec6d050f0d87a&language=pt-BR
 
-function Filme(){
+function Filme() {
 
-    const { id } = useParams(); 
+    const { id } = useParams();
+    const navigate = useNavigate();
+
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        async function loadFilme(){
+        async function loadFilme() {
             await api.get(`/movie/${id}`, {
-                params:{
+                params: {
                     api_key: 'b08cd9bdb025e6a250bec6d050f0d87a',
                     language: 'pt-BR',
                 }
             })
-            .then((response) => {
-                setFilme(response.data);
-                setLoading(false);
-            })
-            .catch(()=>{
-                console.log("FILME NÃO ENCONTRADO");
-            });
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log("FILME NÃO ENCONTRADO");
+                    navigate("/", { replace: true }); //retorna página home
+                    return;
+                });
         }
 
         loadFilme();
 
-        return() => {
+        return () => {
             console.log("COMPONENTE DESMONTADO");
         }
-    }, []);
+    }, [navigate, id]);
 
-    if(loading){
-        return(
+    function salvarFilme() {
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id);
+
+        if (hasFilme) {
+            toast.warn("Esse filme já está na sua lista!");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com sucesso!", {
+            theme: "dark"
+          });
+    }
+
+    if (loading) {
+        return (
             <div className='filme-info'>
                 <h1>Carregando detalhes...</h1>
             </div>
         );
     }
 
-    return(
+    return (
         <div className='filme-info'>
             <h1>{filme.title}</h1>
-            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
 
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a href='#'>
+                    <a target="blank" rel="external" href={`http://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos';
+
 import Header from './components/Header';
+
 import Erro from './pages/Erro';
 
 function RoutesApp(){
@@ -12,6 +15,7 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={ <Home/> } />
                 <Route path='/filme/:id' element={ <Filme/> } />
+                <Route path='/favoritos' element={ <Favoritos/> } />
 
                 <Route path='*' element={ <Erro/> } />
             </Routes>
